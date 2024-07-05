736572adee497e74e1567508c0655a0417520141
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2024-07-05 12:15:25 -0300

    refactor: remove unnecessary interface

--- src/app/utils/providers/index.tsx
+++ src/app/utils/providers/index.tsx
@@ -1,17 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ChildrenType, ProvidersType } from './types';
+import { ProvidersType } from './types';
 import { createTheme, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
+import { PropsWithChildren } from 'react';
 
 const buildProvidersTree = (componentsWithProps: Array<ProvidersType>) => {
-  const initialComponent = ({ children }: ChildrenType) => <>{children}</>;
+  const initialComponent = ({ children }: PropsWithChildren) => <>{children}</>;
   return componentsWithProps.reduce(
     (
       AccumulatedComponents: React.ElementType,
       [Provider, props = {}]: ProvidersType,
     ) => {
-      return ({ children }: ChildrenType) => {
+      return ({ children }: PropsWithChildren) => {
         return (
           <AccumulatedComponents>
             <Provider {...props}>{children}</Provider>
--- src/app/utils/providers/types.ts
+++ src/app/utils/providers/types.ts
@@ -1,5 +1,1 @@
 export type ProvidersType = [React.ElementType, Record<string, unknown>];
-
-export type ChildrenType = {
-  children: Array<React.ElementType>;
-};
