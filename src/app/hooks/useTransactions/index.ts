b4750df13142ed4cb3806313a457f4874b9dea82
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2024-07-06 14:53:55 -0300

    refactor: update transaction filtering logic

--- src/app/hooks/useTransactions/index.ts
+++ src/app/hooks/useTransactions/index.ts
@@ -1,7 +1,14 @@
 import { useQueries } from '@tanstack/react-query';
 import { getPlanets, getTransactions } from '../../services';
 import { UseTransactionsOptions } from './types';
-import { Transaction } from '../../types';
+import {
+  filterByCurrency,
+  filterByMinDate,
+  filterByPlanet,
+  filterByStatus,
+  mapAddingPlanetToTransactions,
+  sortChronologically,
+} from './helpers';
 
 export const useTransactions = ({
   planetId,
@@ -25,55 +32,38 @@ export const useTransactions = ({
     ],
   });
 
-  const sortChronologically = (list: Transaction[]) =>
-    list.sort(({ date: dateA }, { date: dateB }) => {
-      if (dateA < dateB) return 1;
-      if (dateA > dateB) return -1;
-      return 0;
-    });
+  const transactionsWithPlanets = mapAddingPlanetToTransactions(
+    transactionsQuery.data,
+    planetsQuery.data,
+  );
 
-  const jsonDate = minDate?.toJSON();
-  const filterByMinDate = (list: Transaction[]) =>
-    !jsonDate ? list : list.filter(({ date }) => date >= jsonDate);
+  const filteredTransactions = transactionsWithPlanets?.filter(
+    (transaction) => {
+      const isExpectedStatus = status
+        ? filterByStatus(transaction, status)
+        : true;
+      const isExpectedCurrency = currency
+        ? filterByCurrency(transaction, currency)
+        : true;
+      const isExpectedMinDate = minDate
+        ? filterByMinDate(transaction, minDate.toJSON())
+        : true;
+      const isExpectedPlanet = planetId
+        ? filterByPlanet(transaction, planetId)
+        : true;
+      return (
+        isExpectedStatus &&
+        isExpectedCurrency &&
+        isExpectedMinDate &&
+        isExpectedPlanet
+      );
+    },
+  );
 
-  const filterByStatus = (list: Transaction[]) =>
-    !status ? list : list.filter(({ status: tStatus }) => status === tStatus);
-
-  const filterByCurrency = (list: Transaction[]) =>
-    !currency
-      ? list
-      : list.filter(({ currency: tCurrency }) => currency === tCurrency);
-
-  const getPlanetByUser = (userId: string) =>
-    planetsQuery.data?.[
-      planetsQuery.data.findIndex(({ residents }) => residents.includes(userId))
-    ];
-
-  const mapAddingPlanetToTransactions = (list: Transaction[]) =>
-    list?.map((transaction) => {
-      const planet = getPlanetByUser(transaction.user.toString());
-      return {
-        ...transaction,
-        planet,
-      };
-    });
-
-  const filterByPlanet = (list: Transaction[]) =>
-    !planetId ? list : list.filter(({ planet }) => planet?.id === planetId);
-
-  const finalList =
-    transactionsQuery.data &&
-    [
-      sortChronologically,
-      filterByMinDate,
-      filterByStatus,
-      filterByCurrency,
-      mapAddingPlanetToTransactions,
-      filterByPlanet,
-    ].reduce((acc, func) => func(acc), [...transactionsQuery.data]);
+  const finalTransactions = filteredTransactions?.sort(sortChronologically);
 
   return {
     ...transactionsQuery,
-    data: finalList,
+    data: finalTransactions,
   };
 };
--- src/app/services.ts
+++ src/app/services.ts
@@ -1,12 +1,28 @@
 import { api } from './api';
-import { Planet, Transaction } from './types';
+import { Planet, Transaction, TransactionStatus } from './types';
 
-export const getPlanets = (): Promise<Planet[]> => {
-  return api.get('/planets').then((response) => response.data.planets);
-};
+export const getPlanets = (): Promise<Planet[]> =>
+  api.get('/planets').then((response) => response.data.planets);
 
-export const getTransactions = (): Promise<Transaction[]> => {
-  return api
-    .get('/transactions')
+export const getTransactions = (): Promise<Transaction[]> =>
+  api.get('/transactions').then((response) => response.data.transactions);
+
+export const updateTransactions = (
+  transactions: Transaction[],
+  status: TransactionStatus,
+) => {
+  api
+    .put('/transactions/update-batch', {
+      transactions: transactions.map(
+        ({ id, user, amount, currency, date }) => ({
+          id,
+          user,
+          amount,
+          currency,
+          date,
+          status,
+        }),
+      ),
+    })
     .then((response) => response.data.transactions);
 };
