f50ad934d4033789c69850fb1a7dd9d966b0a021
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2024-07-06 01:32:08 -0300

    refactor: usePlanets results filtering

--- src/app/hooks/usePlanets/index.ts
+++ src/app/hooks/usePlanets/index.ts
@@ -2,27 +2,37 @@ import { useQuery } from '@tanstack/react-query';
 import { getPlanets } from '../../services';
 import { UsePlanetsOptions } from './types';
 import { normalizeString } from '../../utils';
+import { Planet } from '../../types';
 
 export const usePlanets = ({ searchQuery = '' }: UsePlanetsOptions = {}) => {
-  const query = useQuery({
+  const planetsQuery = useQuery({
     queryKey: ['planets'],
     queryFn: getPlanets,
   });
 
-  const filteredPlanets =
-    query.data &&
-    [...query.data]
-      .sort(({ name: planetA }, { name: planetB }) => {
-        if (planetA < planetB) return -1;
-        if (planetA > planetB) return 1;
-        return 0;
-      })
-      .filter(({ name }) =>
-        normalizeString(name).includes(normalizeString(searchQuery)),
-      );
+  const sortAlphabetically = (list: Planet[]) =>
+    list.sort(({ name: planetA }, { name: planetB }) => {
+      if (planetA < planetB) return -1;
+      if (planetA > planetB) return 1;
+      return 0;
+    });
+
+  const filterByName = (list: Planet[]) =>
+    !searchQuery
+      ? list
+      : list.filter(({ name }) =>
+          normalizeString(name).includes(normalizeString(searchQuery)),
+        );
+
+  const finalList =
+    planetsQuery.data &&
+    [sortAlphabetically, filterByName].reduce(
+      (acc, func) => func(acc),
+      [...planetsQuery.data],
+    );
 
   return {
-    ...query,
-    data: filteredPlanets,
+    ...planetsQuery,
+    data: finalList,
   };
 };
